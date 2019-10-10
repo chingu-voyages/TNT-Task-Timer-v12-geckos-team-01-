a4d7755ee9ae1574b6a3f7d234b321b59721a447
Nick B <nicholas.balestrieri@gmail.com>
2019-10-10 19:33:39 -0400

    feature: add form fields

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -4,18 +4,36 @@ import {connect} from 'react-redux'
 class NewTask extends Component {
   constructor(props){
     super(props);
-    this.taskName = ""
-    this.tasks = props.tasks
+    this.state = {
+      taskName : "",
+      time: "",
+      date: ""
+    };
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({ taskName: "", time: "" });
   }
 
   render(props) {
     return (
       <div>
-        Name: <input type="text" name="taskName" onChange={this.setState(
-          this.taskName = this.value
-        )} ></input>
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor="taskName">Task Name: </label>
+        <input type="text" name="taskName" onChange={this.handleChange} value={this.state.taskName} ></input>
+        <label htmlFor="date">Completion Date: </label>
+        <input type="date" name="date" onChange={this.handleChange} value={this.state.date}></input>
+        <label htmlFor="time">Completion Time: </label>
+        <input type="time" name="time" onChange={this.handleChange} value={this.state.time}></input>
         <input type="submit" value="Submit"></input>
-      </div>
+        </form>
+        </div>
     )
   }
 }
