fd009b9b6b3f4ad999132ec5911d196231784725
Nick B <nicholas.balestrieri@gmail.com>
2019-10-11 12:50:32 -0400

    feature: implement delete button and connect it with the redux store

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import {createStore} from 'redux';
 
 /* Action types */
 const ADD_TASK = "ADD_TASK"
+const REMOVE_TASK = "REMOVE_TASK"
+
+let nextId = 0;
 
 /* Action Creators */
-let nextId = 0;
 export const addTask = task => ({
   taskName: task.taskName,
   completion: new Date(`${task.date} ${task.time}`).toLocaleString(),
@@ -12,11 +14,17 @@ export const addTask = task => ({
   type: ADD_TASK
 })
 
+export const removeTask = id => ({
+    type: REMOVE_TASK,
+    id: Number(id)
+})
+
 /* Initial State */
 const initialState = {
   tasks: []
 };
 
+/* Reducer */
 const reducer = (state = initialState, action) => {
   switch(action.type){
     case ADD_TASK:
@@ -27,6 +35,8 @@ const reducer = (state = initialState, action) => {
         id
       };
       return {...state, tasks: [...state.tasks, newTask]};
+    case REMOVE_TASK:
+      return {...state, tasks: state.tasks.filter(each => each.id !== action.id)}
     default:
     return state;
   }
