85623b52ae43fa2b5a6daea5e417d14bb634726c
tyler <drelyt@gmail.com>
2019-10-15 16:47:22 -0400

    added react-router-dom and pages

---
+++ src/pages/TaskAlertsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TaskAlertsPage = () => {
+  return (
+    <div>
+      <h1>Viewing Alerts</h1>
+    </div>
+  );
+};
+/* make sure to add PropTypes if any */
+export default TaskAlertsPage;
