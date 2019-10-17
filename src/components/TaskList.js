50467d985f00b36db9e9e3fa50fc8f389cfaae58
tyler <drelyt@gmail.com>
2019-10-17 07:45:43 -0400

    view tasks page redux and styling

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,24 +1,55 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
+
+// bootstrap imports
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+
+// task actions
 import { removeTask } from "../actions/taskActions";
 
-const TaskList = ({ tasks, remove }) => (
-  <div>
-    <ul>
-      {tasks.map(task => (
-        <div key={task.id}>
-          <h4>{task.taskName}</h4>
-          <p>To be completed: {task.completion}</p>
-          <Button id={task.id} onClick={remove}>
-            Remove
-          </Button>
-        </div>
-      ))}
-    </ul>
-  </div>
-);
+// utilities
+import { convertSecondsToHMS } from "../util/timetools";
+
+const TaskList = ({ tasks, remove }) => {
+  return (
+    <div>
+      <ListGroup>
+        {tasks.map(task => {
+          const duration = convertSecondsToHMS(task.duration);
+
+          // add leading 0 to any numbers less than 10
+          const hours =
+            duration.hours < 10 ? `0${duration.hours}` : duration.hours;
+          const minutes =
+            duration.minutes < 10 ? `0${duration.minutes}` : duration.minutes;
+          const seconds =
+            duration.seconds < 10 ? `0${duration.seconds}` : duration.seconds;
+          const durationString = `${hours}:${minutes}:${seconds}`;
+
+          return (
+            <ListGroup.Item key={task.id}>
+              <div className="task-list-item">
+                <h4 className="task-list-name">{task.taskName}</h4>
+                <h5 className="task-list-duration">
+                  Duration: {durationString}
+                </h5>
+                <Button
+                  className="task-list-remove-button"
+                  id={task.id}
+                  onClick={remove}
+                >
+                  Remove
+                </Button>
+              </div>
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+    </div>
+  );
+};
 
 TaskList.propTypes = {
   remove: PropTypes.any.isRequired,
@@ -26,7 +57,7 @@ TaskList.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  tasks: state.tasks
+  tasks: state.tasks.tasks
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/reducer/taskReducer.js
+++ src/reducer/taskReducer.js
@@ -1,5 +1,24 @@
 import { ADD_TASK, REMOVE_TASK } from "../actions/types";
 
+// create some fake tasks for testing
+// duration is in seconds
+const testTasks = [
+  { taskName: "Reading a book", date: new Date(), duration: 6000 },
+  { taskName: "Working on project", date: new Date(), duration: 4240 },
+  { taskName: "Watching TV", date: new Date(), duration: 2400 },
+  { taskName: "Eating Lunch", date: new Date(), duration: 1001 },
+  { taskName: "Napping", date: new Date(), duration: 985 },
+  { taskName: "Refactoring app", date: new Date(), duration: 720 }
+];
+
+// set some ids for the tasks
+testTasks.forEach((task, index) => {
+  // eslint-disable-next-line no-param-reassign
+  task.id = index;
+});
+// set the tasks in the localstorage
+localStorage.setItem("tasks", JSON.stringify(testTasks));
+
 /* Initial State */
 const tasksFromLocalState = JSON.parse(localStorage.getItem("tasks")) || [];
 const highestId = tasksFromLocalState.length
