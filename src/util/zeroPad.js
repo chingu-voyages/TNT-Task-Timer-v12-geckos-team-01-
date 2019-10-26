0b810263eb980dba8d2ceffa6e35b9aaed2184a4
Thomas Herzog <Tomrule007@gmail.com>
2019-10-25 17:27:15 -0700

    fix accidental capital letter in zeroPad util

--- src/util/zeroPad.js
+++ src/util/zeroPad.js
@@ -1,4 +1,4 @@
-// ZeroPad adds a leading '0' if input is less than 10 and returns it
-const ZeroPad = input => `${+input < 10 ? '0' : ''}${input}`;
+// zeroPad adds a leading '0' if input is less than 10 and returns it
+const zeroPad = input => `${+input < 10 ? '0' : ''}${input}`;
 
-export default ZeroPad;
+export default zeroPad;
