acbbef5f6f68d34220e41e882f9d104bf9402a36
Thomas Herzog <Tomrule007@gmail.com>
2019-10-30 15:47:03 -0700

    Refactor reportsPage to pass props to TaskChart.
    Also imported mockdata to provide a mock chart along with the real one.

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import TaskChart from '../components/TaskChart';
+import chartFormats from '../util/chartData';
+import { getOneHourTasks } from '../util/mockTaskData';
 
-const ReportsPage = () => {
+const ReportsPage = ({ taskList }) => {
   return (
     <div>
       <h1>Viewing Reports</h1>
-      <TaskChart />
+      <TaskChart
+        data={chartFormats.getTaskTotals(taskList)}
+        title="Total Task Time"
+        xKey="taskName"
+        xTitle="Task Name"
+        yKey="seconds"
+        yTitle="Time [seconds]"
+      />
+      <TaskChart
+        data={chartFormats.getTaskTotals(getOneHourTasks())}
+        title="Mock Data Total Task Time"
+        xKey="taskName"
+        xTitle="Task Name"
+        yKey="seconds"
+        yTitle="Time [seconds]"
+      />
     </div>
   );
 };
 /* make sure to add PropTypes if any */
-export default ReportsPage;
+
+ReportsPage.propTypes = {
+  taskList: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    taskList: state.tasks.taskList
+  };
+};
+export default connect(mapStateToProps)(ReportsPage);
