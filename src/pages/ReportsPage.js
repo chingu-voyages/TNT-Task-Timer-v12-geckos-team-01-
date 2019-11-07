646c06cfc278374f271f2b0b739780331d8750d2
Thomas Herzog <Tomrule007@gmail.com>
2019-11-07 13:49:05 -0800

    add total time display to report page

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import TaskChart from '../components/TaskChart';
 import chartFormats from '../util/chartData';
+import taskOperations from '../util/taskOperations';
 import { getOneHourTasks } from '../util/mockTaskData';
 
 const ReportsPage = ({ taskList }) => {
+  const TotalMinutes =
+    taskList.reduce((acc, task) => acc + taskOperations.totalTime(task), 0) /
+    60;
+  const TotalDisplay =
+    TotalMinutes < 60
+      ? `${TotalMinutes.toFixed(1)} minutes`
+      : `${(TotalMinutes / 60).toFixed(1)} hours`;
   return (
     <div>
-      <h1>Viewing Reports</h1>
+      <h2 style={{ textAlign: 'right' }}>{TotalDisplay} tracked total</h2>
       <TaskChart
         data={chartFormats.getTaskTotals(taskList)}
         title="Total Task Time"
